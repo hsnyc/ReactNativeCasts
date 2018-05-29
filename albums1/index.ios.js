ce6e8cdef091a6e1a8eeeaa68a3a2dc0dd605c77
Henry <hsanchez@healthesystems.com>
2018-05-29 12:04:50 -0400

    album list component

--- albums1/index.ios.js
+++ albums1/index.ios.js
@@ -1,19 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry
-} from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 export default class albums1 extends Component {
   render() {
     return (
-      <Header headerText={'Albums'} />
+      <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
     );
   }
 }
---
+++ albums1/src/components/AlbumList.js
@@ -0,0 +1,13 @@
+// Import libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AlbumList = () => {
+    return (
+        <View>
+            <Text>Album List!! </Text>
+        </View>
+    );
+};
+
+export default AlbumList;
