6a8e536b446954b3e9b458ed86321a4a639c813c
Henry <hsanchez@healthesystems.com>
2018-05-29 11:23:57 -0400

    style changes for header.

--- albums1/index.ios.js
+++ albums1/index.ios.js
@@ -6,51 +6,16 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry
 } from 'react-native';
 import Header from './src/components/header';
 
 export default class albums1 extends Component {
   render() {
     return (
-      <View style={styles.container}>
-
-        <Header />
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Header />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('albums1', () => albums1);
--- albums1/src/components/header.js
+++ albums1/src/components/header.js
@@ -4,14 +4,27 @@ import { Text, View } from 'react-native';
 
 // Make a component
 const Header = () => {
-    return <Text style={styles.textStyle}>All Albums!</Text>;
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>All Albums!</Text>
+        </View>
+    ); 
 };
 
 // Component styles
 const styles = {
+    viewStyle: {
+        backgroundColor: '#f8f8f8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15
+    },
     textStyle: {
         fontSize: 20,
-        color: 'green'
+        color: '#333333'
     }
 };
 
