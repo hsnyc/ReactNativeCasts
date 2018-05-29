e343302ee4b5d9c5351e00c4ce081cdd4cc74075
Henry <hsanchez@healthesystems.com>
2018-05-29 16:16:26 -0400

    image styles for cover.

--- albums1/src/components/AlbumDetail.js
+++ albums1/src/components/AlbumDetail.js
@@ -7,23 +7,31 @@ import CardSection from './CardSection';
 //Class Based Componet
 const AlbumDetail = ({ album }) => {
     //de-structure props and styles object
-    const { title, artist, thumbnail_image } = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
-
+    const { title, artist, thumbnail_image, image } = album;
+    const {
+        thumbnailStyle,
+        headerContentStyle,
+        thumbnailContainerStyle,
+        headerTextStyle,
+        imageStyle 
+    } = styles;
 
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image 
                         source={{ uri: thumbnail_image }}
                         style={thumbnailStyle}
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+            <CardSection>
+                <Image style={imageStyle} source={{ uri: image }} />
             </CardSection>        
         </Card>
     );
@@ -34,9 +42,23 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
