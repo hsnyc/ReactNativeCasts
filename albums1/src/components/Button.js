1dbb606e6f8f2f81519b23452a8c394df20febad
Henry <hsanchez@healthesystems.com>
2018-05-30 09:33:56 -0400

    added styles for button.

--- albums1/src/components/Button.js
+++ albums1/src/components/Button.js
@@ -3,11 +3,35 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+
+    const { buttonStyles, textStyles } = styles;
+
     return (
-        <TouchableOpacity>
-            <Text>Click Me!</Text>
+        <TouchableOpacity style={buttonStyles}>
+            <Text style={textStyles}>Click Me!</Text>
         </TouchableOpacity>
     );
+};
+
+const styles = {
+    buttonStyles: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
+    },
+    textStyles: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    }
 };
 
 export default Button;
