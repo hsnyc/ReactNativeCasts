2e4d9dd8089fc817ca571e240270e387250e9a53
Henry <hsanchez@healthesystems.com>
2018-06-01 16:14:27 -0400

    firebase installed

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,8 +2,21 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+    //lifecycle method
+    conponentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyC-c7u8BlHC2WFYbd6AFXiF1ikvOHPClIA",
+            authDomain: "authentication-798a0.firebaseapp.com",
+            databaseURL: "https://authentication-798a0.firebaseio.com",
+            projectId: "authentication-798a0",
+            storageBucket: "authentication-798a0.appspot.com",
+            messagingSenderId: "323725003005"
+        });
+    }
+
     render() {
         return (
             <View>
