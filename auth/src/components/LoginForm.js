9c3a0e6fed1901292dfe633f0ac2a2175e8b410b
Henry <hsanchez@healthesystems.com>
2018-06-01 16:49:17 -0400

    input created.

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 //Class Based Componet
 class LoginForm extends Component {
+
+    state = { text: '' };
+
     render() {
         return (
             <Card>
-                <CardSection />
+                <CardSection>
+                    <TextInput style={{ height: 40, width: 100 }} />
+                </CardSection>
                 <CardSection />
                 <CardSection>
                     <Button>Login</Button>
