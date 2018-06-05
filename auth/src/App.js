5df8e464c7dffefac7f30b5de1fdf555233409af
Henry <hsanchez@healthesystems.com>
2018-06-05 15:35:16 -0400

    logging state changed logic.

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,13 @@
 //Functional Componet
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button } from './components/common';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: false };
+
     //lifecycle method
     componentWillMount() {
         firebase.initializeApp({
@@ -16,13 +18,33 @@ class App extends Component {
             storageBucket: "authentication-798a0.appspot.com",
             messagingSenderId: "323725003005"
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        if(this.state.loggedIn) {
+            return (
+                <Button>
+                    Log Out
+                </Button>
+            );
+        }
+
+        return <LoginForm />;
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
