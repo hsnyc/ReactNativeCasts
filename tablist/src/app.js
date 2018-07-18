8565534ab8620bc232f8514c70f80b34e93a7bb3
Henry <hsanchez@healthesystems.com>
2018-07-18 12:58:19 -0400

    installed redux and react-redux (duh!)

--- tablist/src/app.js
+++ tablist/src/app.js
@@ -3,11 +3,14 @@ import { View } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
     return (
         <Provider store={createStore(reducers)}>
-            <View />
+            <View>
+                <Header headerText="Tech Stack" />
+            </View>
         </Provider>
     );
 };
