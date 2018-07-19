c91a4e9a4d15c71df3fe9cd03108d37e901fde18
Henry <hsanchez@healthesystems.com>
2018-07-19 12:23:13 -0400

    added styles to text.

--- tablist/src/components/ListItem.js
+++ tablist/src/components/ListItem.js
@@ -3,12 +3,25 @@ import { Text } from 'react-native';
 import { CardSection } from './common';
 
 class ListItem extends Component {
+
     render() {
+        
+        const { titleStyle } = styles;
+
         return (
             <CardSection>
-                <Text>{this.props.library.item.title}</Text>
+                <Text style={titleStyle}>
+                    {this.props.library.item.title}
+                </Text>
             </CardSection>
         );
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
     }
 }
 
