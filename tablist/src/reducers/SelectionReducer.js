032a0ebd9ffe51f5bcd5def7dd2e0af61705f8e5
Henry <hsanchez@healthesystems.com>
2018-07-19 15:27:02 -0400

    added return payload to reducer.

--- tablist/src/reducers/SelectionReducer.js
+++ tablist/src/reducers/SelectionReducer.js
@@ -1,4 +1,10 @@
-export default (state, action) => {
+export default (state = null, action) => {
     //console.log(action);
-    return null;
+    switch (action.type) {
+        case 'select_library':
+            return action.payload;
+    
+        default: 
+            return state;
+    }
 };
