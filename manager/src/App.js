26235fb37f269cbfcae78b46ba2f79936cbfe339
Henry <hsanchez@healthesystems.com>
2018-07-20 12:08:17 -0400

    added firebase and eslint.

--- manager/src/App.js
+++ manager/src/App.js
@@ -5,6 +5,20 @@ import { Provider } from 'react-redux';
 import reducers from './reducers';
 
 class App extends Component {
+
+    componentWillMount() {
+        // Initialize Firebase
+        const config = {
+            apiKey: 'AIzaSyD8E9WUgG2bunNVdxRTJOChx08U5EtHCyc',
+            authDomain: 'manager-320b0.firebaseapp.com',
+            databaseURL: 'https://manager-320b0.firebaseio.com',
+            projectId: 'manager-320b0',
+            storageBucket: '',
+            messagingSenderId: '432483403158'
+        };
+        firebase.initializeApp(config);
+    }
+
     render () {
         return (
             //Provider is what connects to the connect tags and makes sure those tags have access to the store to grab the redux state and passes to all the components in the app.
