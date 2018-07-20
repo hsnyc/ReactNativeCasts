275fe7343d4d9b7ccd9d3a40ec01b8c091ac04c6
Henry <hsanchez@healthesystems.com>
2018-07-20 12:14:02 -0400

    import fb. Also rolled back as it was not compiling on Android.

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
             storageBucket: '',
             messagingSenderId: '432483403158'
         };
+        
         firebase.initializeApp(config);
     }
 
